4510633a4aaa7129f9e5942401aafa416444c3d6
pbrosamond <pbrosamond@gmail.com>
2024-02-01 00:44:43 -0800

    Front-End completed. May need some bug fixes

--- client/src/components/Videos/Videos.js
+++ client/src/components/Videos/Videos.js
@@ -1,9 +1,19 @@
 import "./Videos.scss";
+import poster from "../../assets/images/posters/Brave.jpeg"
 
 const Videos = () => {
     return (
       <section className="videos">
-
+        <p className="videos__title">NEW AND NOTEWORTHY</p>
+        <div className="videos__list">
+          <img className="videos__poster" src={poster} alt="Movie Poster"></img>
+          <img className="videos__poster" src={poster} alt="Movie Poster"></img>
+          <img className="videos__poster" src={poster} alt="Movie Poster"></img>
+          <img className="videos__poster" src={poster} alt="Movie Poster"></img>
+          <img className="videos__poster" src={poster} alt="Movie Poster"></img>
+          <img className="videos__poster" src={poster} alt="Movie Poster"></img>
+          <img className="videos__poster" src={poster} alt="Movie Poster"></img>
+        </div>
       </section>
     );
 };
