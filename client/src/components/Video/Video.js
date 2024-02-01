1c73467d37ecee0ece9a15963e6d2ea233ed32db
Rajandeep56 <rajandeepkaur61@outlook.com>
2024-02-01 13:52:44 -0800

    test

--- client/src/components/Video/Video.js
+++ client/src/components/Video/Video.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 // import poster from "../../assets/images/posters/Brave.jpeg"
 
 
-const Video = ({title}) => {
+const Video = ({poster}) => {
   return (
     
     
-    // <img
-    //   className="videos__poster"
-    //   src={poster}
-    //   width="80%"
-    //   alt="Movie Poster"/>
-    <h1>title</h1>
+    <img
+      className="videos__poster"
+      src={poster}
+      width="80%"
+      alt="Movie Poster"/>
+    
    
 
   );
